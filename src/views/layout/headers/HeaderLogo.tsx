c04f5c12e9ae430bd057b4fb33ae49c9b57be6a0
Saeed <appsaeed7@gmail.com>
2023-06-24 13:55:59 +0600

    update logo with image and text

--- src/views/layout/headers/HeaderLogo.tsx
+++ src/views/layout/headers/HeaderLogo.tsx
@@ -1,36 +1,28 @@
 import { ReactNode } from "react";
 import { Link } from "react-router-dom";
 import settings from "../../../app/settings";
-import { usePreference } from "../../../context/PreferenceContext";
 
 export default function HeaderLogo({ children }: { children?: ReactNode }) {
-  const preference = usePreference();
-  const iconName =
-    preference.sidebarType === "full" ? (
-      settings.name
-    ) : (
-      <img src={settings.logo || ""} alt="homepage" className="light-logo" />
-    );
-
   return (
     <Link className="navbar-brand" to="/">
-      <b className="logo-icon">
-        {/* <img
+      {/* <b className="logo-icon">
+        <img
           src={settings.logo || ""}
           alt={settings.name}
           className="dark-logo"
-        /> */}
-        {/* <img
+        />
+        <img
           src={settings.logo || ""}
           alt={settings.name}
           className="light-logo"
-        /> */}
+        />
+      </b> */}
+      <b>
+        <img src={settings.logo || ""} alt="homepage" />
       </b>
-      <span className="logo-text">
-        <img src={settings.logo || ""} alt="homepage" className="dark-logo" />
-        <img src={settings.logo || ""} alt="homepage" className="light-logo" />
+      <span className="logo-text text-light text-center w-100">
+        {settings.name}
       </span>
-      <div className="text-light text-center w-100"> {iconName}</div>
       {children}
     </Link>
   );
